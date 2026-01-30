d95203ca1f284f8757203cdeb83c7e4f1367b5cc
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2026-01-30 18:55:37 +0800

    feat: Added ability to delete and toggle publish state of blogposts

--- src/components/blogs.jsx
+++ src/components/blogs.jsx
@@ -18,19 +18,64 @@ export default function Blogs() {
     getBlogs();
   }, []);
 
-  function generateBlogEntry({ id, title, creationDate }) {
+  function onTogglePublish(index, id) {
+    return async () => {
+      const response = await axios.post(`http://localhost:3000/blogs/${id}`, {
+        withCredentials: true,
+      });
+
+      if (!response?.data?.error) {
+        const newState = { ...blogs };
+        newState.blogs[index].published = response?.data?.published;
+        setBlogs(newState);
+      }
+    };
+  }
+
+  function onDelete(index, id) {
+    return async () => {
+      const response = await axios.delete(`http://localhost:3000/blogs/${id}`, {
+        withCredentials: true,
+      });
+
+      console.log(response);
+
+      if (!response?.data?.error) {
+        const newState = { ...blogs };
+        console.log(newState);
+        newState.blogs.splice(index, 1);
+        setBlogs(newState);
+      }
+    };
+  }
+
+  function generateBlogEntry(index, { id, title, creationDate, published }) {
     return (
-      <Link key={id} className="blogEntry" to={`/blogs/${id}`} viewTransition>
-        <p className="blogTitle">{title}</p>
-        <p className="publishDate">{new Date(creationDate).toDateString()}</p>
-      </Link>
+      <div key={id} className="blogEntry">
+        <Link className="blogDetails" to={`/blogs/${id}`} viewTransition>
+          <p className="blogTitle">{title}</p>
+          <p className="publishDate">{new Date(creationDate).toDateString()}</p>
+        </Link>
+        <div className="buttons">
+          <button
+            className={published ? "negative" : ""}
+            type="button"
+            onClick={onTogglePublish(index, id)}
+          >
+            {published ? "Unpublish" : "Publish"}
+          </button>
+          <button className="negative" onClick={onDelete(index, id)}>
+            Delete
+          </button>
+        </div>
+      </div>
     );
   }
 
   function displayBlogs() {
     if (blogs?.blogs) {
-      const formattedBlogs = blogs.blogs.map((blog) => {
-        return generateBlogEntry(blog);
+      const formattedBlogs = blogs.blogs.map((blog, index) => {
+        return generateBlogEntry(index, blog);
       });
       return <div className="blogList">{formattedBlogs}</div>;
     } else if (blogs?.error) {
